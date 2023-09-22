8e35d17c006efd2e72eb59945cec86d49c22064a
n-wey <142606400+n-wey@users.noreply.github.com>
2023-09-22 16:20:35 +0200

    Add files via upload

---
+++ Guess Number/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Guess_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+            bool correct = false;
+            int attempt = 0;
+            int versuch = 0;
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine("Pick a number between 1 and 100");
+            string guess = Console.ReadLine();
+            while (!correct && versuch < 1)
+            {
+                if (!int.TryParse(guess, out attempt))
+                { continue; }
+
+                if (attempt > number)  {
+                    Console.WriteLine("zu hoch");
+                    versuch++;
+                    
+                }
+                if (attempt < number)
+                {
+                    Console.WriteLine("zu tief");
+                    versuch++;
+                    
+                {
+                if (attempt == number) 
+                            Console.WriteLine("Dieser Versuch stimmt");
+                }
+               
+                }
+            }
+
+        }
+    }
+}
